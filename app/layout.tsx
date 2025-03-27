31d1d6f66427ddf545f96f7a3be05fef6522ce4a
Michal Gawik <gawlik.zator@o2.pl>
2025-03-27 12:41:34 +0100

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Montserrat, Calistoga } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
+const calistoga = Calistoga({
+	variable: '--font-calistoga',
 	subsets: ['latin'],
+	weight: '400',
 })
 
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
+const montserrat = Montserrat({
+	variable: '--font-montserrat',
 	subsets: ['latin'],
 })
 
@@ -25,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				className={`${calistoga.variable} ${montserrat.variable}   antialiased`}>
 				{children}
 			</body>
 		</html>
