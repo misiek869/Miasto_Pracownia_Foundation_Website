f1701744914e3fcaec5618d32b31d22aa20e1208
Michal Gawik <gawlik.zator@o2.pl>
2025-04-03 06:14:30 +0200

    create title component

---
+++ components/PageTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageTitle = ({ title }: { title: string }) => {
+	return (
+		<h1 className='mx-auto text-center  font-calistoga tracking-wide leading-snug text-5xl md:text-6xl text-rose-500 uppercase'>
+			{title}
+		</h1>
+	)
+}
+
+export default PageTitle
