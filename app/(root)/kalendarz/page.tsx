a355a6659eb5ecba96dbcbb2e6a51bf2243ef1da
Michal Gawik <gawlik.zator@o2.pl>
2025-04-06 06:05:33 +0200

    add google map to page

--- app/(root)/kalendarz/page.tsx
+++ app/(root)/kalendarz/page.tsx
@@ -4,71 +4,16 @@ import PageTitle from '@/components/PageTitle'
 
 const CalendarPage = () => {
 	return (
-		<div className='px-8 bg-[var(--primary)] pt-20 min-h-screen '>
+		<>
 			<div className='absolute top-6 left-6'>
 				<Navigation hidden='calendar' />
 			</div>
-			<PageTitle title='kalendarz warsztatów' />
-			<Calendar />
-		</div>
+			<div className='px-8 bg-[var(--primary)] pt-20 min-h-screen '>
+				<PageTitle title='kalendarz warsztatów' />
+				<Calendar />
+			</div>
+		</>
 	)
 }
 
 export default CalendarPage
-
-{
-	/* <div
-								key={uuidv4()}
-								className='h-14 md:h-21 lg:h-28 border-t grid place-content-center text-sm relative'>
-								<h1
-									className={cn(
-										currentMonth ? '' : 'text-gray-400',
-										today
-											? 'border-2 border-rose-500 rounded-full text-black'
-											: '',
-
-										// selectDate.toDate().toDateString() ===
-										// 	date.toDate().toDateString()
-										// 	? 'bg-blue-500 text-white'
-										// 	: '',
-										'h-10 w-10 grid place-content-center lg:text-xl transition-all rounded-full ',
-										event ? 'font-semibold' : ''
-									)}
-									style={{
-										backgroundColor: event ? eventColor : '',
-										color: event ? 'white' : '',
-									}}
-									onClick={() => {
-										setSelectDate(date)
-										setEvent(event)
-									}}>
-									{date.date()}
-								</h1>
-								{event ? (
-									<div className='hidden lg:block mx-auto absolute right-3 top-3.5 '>
-										<Popover>
-											<PopoverTrigger>
-												<LuMessageCircleMore className='w-7 h-7 cursor-pointer text-rose-500' />
-											</PopoverTrigger>
-											<PopoverContent>
-												<div className=''>
-													<h1 className='font-semibold text-sm'>
-														{event.name} {event.eventDate}
-													</h1>
-													<p className='text-sm'>godz: {event.eventHour}</p>
-													<p className='text-xs mt-4'>{event.description}</p>
-													<Button
-														className='mt-4'
-														variant={'outline'}
-														size={'sm'}>
-														<Link href={'#'}>Zapisz się na warsztat</Link>
-													</Button>
-												</div>
-											</PopoverContent>
-										</Popover>
-									</div>
-								) : (
-									''
-								)}
-							</div> */
-}
--- app/(root)/kontakt/page.tsx
+++ app/(root)/kontakt/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+
 import React from 'react'
+import Navigation from '@/components/Navigation'
+import PageTitle from '@/components/PageTitle'
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps'
 
 const ContactPage = () => {
-	return <div></div>
+	const position = { lat: 50.0387346443218, lng: 19.226098300000004 }
+
+	return (
+		<>
+			<div className='absolute top-6 left-6'>
+				<Navigation hidden='contact' />
+			</div>
+			<div className='px-8 bg-[var(--primary)] py-20 min-h-screen w-screen'>
+				<PageTitle title='kontakt' />
+				<div className='grid grid-cols-2 pt-20'>
+					<APIProvider
+						apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY || ''}>
+						<div className='h-[50vh] w-[100%]'>
+							<Map
+								defaultCenter={position}
+								defaultZoom={10}
+								mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+								<AdvancedMarker position={position} />
+							</Map>
+						</div>
+					</APIProvider>
+					<div className=''></div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default ContactPage
