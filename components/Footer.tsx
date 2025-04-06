932a63e24ae26726490618f6954b70e2f26e33f5
Michal Gawik <gawlik.zator@o2.pl>
2025-04-06 06:54:51 +0200

    create footer

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import dayjs from 'dayjs'
+import Link from 'next/link'
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+
+const Footer = () => {
+	const currentYear = dayjs().year()
+	console.log(currentYear)
+
+	return (
+		<div className='bg-rose-500 flex-row md:flex justify-center md:justify-between  py-6 px-8 md:py-4'>
+			<p className='text-slate-50 tracking-wider font-montserrat text-lg font-normal mb-4 md:mb-0 text-center'>
+				Copyright &copy;{currentYear} Miasto Pracownia
+			</p>
+			<div className='flex items-center gap-x-4 justify-center'>
+				<Link href={'#'}>
+					<FaFacebook className='text-slate-50 w-7 h-7 hover:text-slate-200 hover:animate-pulse' />
+				</Link>
+				<Link href={'#'}>
+					<FaInstagram className='text-slate-50 w-7 h-7 hover:text-slate-200 hover:animate-pulse' />
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Footer
