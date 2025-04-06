befeb566f0218dd535186df48e9bf686239a26fe
Michal Gawik <gawlik.zator@o2.pl>
2025-04-06 07:43:35 +0200

    change tracking

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageTitle = ({ title }: { title: string }) => {
 	return (
-		<h1 className='mx-auto text-center  font-calistoga tracking-wide leading-snug text-5xl md:text-6xl text-rose-500 uppercase'>
+		<h1 className='mx-auto text-center  font-calistoga tracking-wider leading-snug text-5xl md:text-6xl text-rose-500 uppercase'>
 			{title}
 		</h1>
 	)
