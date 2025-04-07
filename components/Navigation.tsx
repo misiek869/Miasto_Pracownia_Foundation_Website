3f81b53fe55a6f861cb98d4bdd5a990eeeceafec
Michal Gawik <gawlik.zator@o2.pl>
2025-04-07 14:07:45 +0200

    change navigation styling

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,15 +11,14 @@ type NavigationProps = {
 
 const Navigation = ({ disabled, hidden }: NavigationProps) => {
 	return (
-		<header className='flex w-full shrink-0 items-center'>
-			<Sheet>
-				<SheetTrigger asChild>
-					<Button variant='outline' size='icon' className='rounded-full'>
-						<Menu className='h-6 w-6' />
-					</Button>
-				</SheetTrigger>
-				<SheetContent side='left'>
-					{disabled ? (
+		<Sheet>
+			<SheetTrigger asChild>
+				<Button variant='outline' size='icon' className='rounded-full'>
+					<Menu className='h-6 w-6' />
+				</Button>
+			</SheetTrigger>
+			<SheetContent side='left' className='h-[300px] w-[300px] rounded-full'>
+				{/* {disabled ? (
 						<div className='ml-6 mt-4 flex'>
 							<GiFlamingo className='h-12 w-12 text-rose-500' />
 						</div>
@@ -27,60 +26,59 @@ const Navigation = ({ disabled, hidden }: NavigationProps) => {
 						<Link href='/' className='ml-6 mt-4 flex' prefetch={false}>
 							<GiFlamingo className='h-12 w-12 text-rose-500' />
 						</Link>
-					)}
+					)} */}
 
-					<div className='grid gap-2 py-6 ml-6'>
-						{/* <Link
+				<div className='grid gap-2 py-6 justify-center '>
+					{/* <Link
 							href='#'
 							className='flex w-full items-center py-2 text-lg font-semibold'
 							prefetch={false}>
 							Home
 						</Link> */}
 
-						<Link
-							href='/o_nas'
-							className={
-								hidden === 'about'
-									? 'hidden'
-									: 'flex w-full items-center py-2 text-lg font-semibold'
-							}
-							prefetch={false}>
-							O Nas
-						</Link>
-						<Link
-							href='/pracownie'
-							className={
-								hidden === 'workshops'
-									? 'hidden'
-									: 'flex w-full items-center py-2 text-lg font-semibold'
-							}
-							prefetch={false}>
-							Pracownie
-						</Link>
-						<Link
-							href='/kalendarz'
-							className={
-								hidden === 'calendar'
-									? 'hidden'
-									: 'flex w-full items-center py-2 text-lg font-semibold'
-							}
-							prefetch={false}>
-							Kalendarz
-						</Link>
-						<Link
-							href='/kontakt'
-							className={
-								hidden === 'contact'
-									? 'hidden'
-									: 'flex w-full items-center py-2 text-lg font-semibold'
-							}
-							prefetch={false}>
-							Kontakt
-						</Link>
-					</div>
-				</SheetContent>
-			</Sheet>
-		</header>
+					<Link
+						href='/o_nas'
+						className={
+							hidden === 'about'
+								? 'hidden'
+								: 'text-center py-2 text-xl font-semibold'
+						}
+						prefetch={false}>
+						O Nas
+					</Link>
+					<Link
+						href='/pracownie'
+						className={
+							hidden === 'workshops'
+								? 'hidden'
+								: 'text-center py-2 text-xl font-semibold'
+						}
+						prefetch={false}>
+						Pracownie
+					</Link>
+					<Link
+						href='/kalendarz'
+						className={
+							hidden === 'calendar'
+								? 'hidden'
+								: 'text-center py-2 text-xl font-semibold'
+						}
+						prefetch={false}>
+						Kalendarz
+					</Link>
+					<Link
+						href='/kontakt'
+						className={
+							hidden === 'contact'
+								? 'hidden'
+								: 'text-center py-2 text-xl font-semibold'
+						}
+						prefetch={false}>
+						Kontakt
+					</Link>
+				</div>
+			</SheetContent>
+		</Sheet>
 	)
 }
 
