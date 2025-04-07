141f3fa3080e32a66c2d18bcf15d12d237dbfdf2
Michal Gawik <gawlik.zator@o2.pl>
2025-04-07 11:44:16 +0200

    create file

---
+++ components/carousel/Carousel.tsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import AutoScroll from 'embla-carousel-auto-scroll'
+// import {
+//   NextButton,
+//   PrevButton,
+//   usePrevNextButtons
+// } from './EmblaCarouselArrowButtons'
+
+type PropType = {
+	slides: number[]
+	options?: EmblaOptionsType
+}
+
+const Carousel = () => {
+	return <div></div>
+}
+
+export default Carousel
