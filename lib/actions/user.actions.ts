45fef4f061f8ef60f3da1736155b8ce8f5e2f19a
Michal Gawik <gawlik.zator@o2.pl>
2025-04-09 08:36:41 +0200

    create signInWithCredentials action and signOutUser action

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,28 @@
+'use server'
+
+import { signInFormSchema } from '../validators'
+import { signIn, signOut } from '@/auth'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+
+export async function signInWithCredentials(prev: unknown, formData: FormData) {
+	try {
+		const user = signInFormSchema.parse({
+			email: formData.get('email'),
+			password: formData.get('password'),
+		})
+
+		await signIn('credentials', user)
+
+		return { success: true, message: 'Signed in successfully' }
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error
+		}
+
+		return { success: false, message: 'Invalid email or password' }
+	}
+}
+
+export async function signOutUser() {
+	await signOut()
+}
