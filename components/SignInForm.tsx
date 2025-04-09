618810ac3af3b32d9c4efda34e648db095cd05d2
Michal Gawik <gawlik.zator@o2.pl>
2025-04-09 09:19:38 +0200

    add useActionState and useFormStatus

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,7 +1,61 @@
 'use client'
 
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { signInWithCredentials } from '@/lib/actions/user.actions'
+import { useActionState } from 'react'
+import { useFormStatus } from 'react-dom'
+
 const SignInForm = () => {
-	return <div>Form</div>
+	const [data, action] = useActionState(signInWithCredentials, {
+		success: false,
+		message: '',
+	})
+
+	const SingInButton = () => {
+		const { pending } = useFormStatus()
+		return (
+			<Button
+				className='w-full text-gray-900 cursor-pointer'
+				disabled={pending}>
+				{pending ? 'Logowanie' : 'Zaloguj'}
+			</Button>
+		)
+	}
+
+	return (
+		<form action={action}>
+			<div className='space-y-6'>
+				<div>
+					<Label htmlFor='email'>Email</Label>
+					<Input
+						id='email'
+						name='email'
+						type='email'
+						required
+						autoComplete='email'
+						defaultValue={'michal@michal.pl'}
+					/>
+				</div>
+				<div>
+					<Label htmlFor='password'>Hasło</Label>
+					<Input
+						id='password'
+						name='password'
+						type='password'
+						required
+						autoComplete='password'
+						defaultValue={'123456'}
+					/>
+				</div>
+				<div className=''>
+					<SingInButton />
+				</div>
+				{data && !data.success && <div className=''>{data.message}</div>}
+			</div>
+		</form>
+	)
 }
 
 export default SignInForm
