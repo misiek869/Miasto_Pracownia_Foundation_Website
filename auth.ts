1a6be60f01a36e17eb610d31bd920d2032dd8b3f
Michal Gawik <gawlik.zator@o2.pl>
2025-04-09 12:11:16 +0200

    create callback

--- auth.ts
+++ auth.ts
@@ -54,7 +54,26 @@ export const config = {
 	callbacks: {
 		async session({ session, user, trigger, token }: any) {
 			session.user.id = token.sub
+			session.user.role = token.role
+			session.user.name = token.name
+
 			return session
+		},
+		async jwt({ token, user, trigger, session }: any) {
+			if (user) {
+				token.role = user.role
+
+				if (user.name === 'NO_NAME') {
+					token.name = user.email!.split('@')[0]
+
+					await prisma.user.update({
+						where: { id: user.id },
+						data: { name: token.name },
+					})
+				}
+			}
+
+			return token
 		},
 	},
 } satisfies NextAuthConfig
