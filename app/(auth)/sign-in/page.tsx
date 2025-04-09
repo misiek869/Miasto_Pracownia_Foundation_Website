9dcb7034c78e1f00ef65b341aecb76b9ce59e351
Michal Gawik <gawlik.zator@o2.pl>
2025-04-09 08:49:30 +0200

    change layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,13 +1,21 @@
+import { Card, CardHeader } from '@/components/ui/card'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
 	title: 'Sign In',
 }
 
 const SignInPage = () => {
-	return <div className='w-full max-w-md mx-auto'>
-		<
-	</div>
+	return (
+		<div className='w-full max-w-md mx-auto'>
+			<Card>
+				<CardHeader className='space-y-4'>
+					<Link href={'/'} className=''></Link>
+				</CardHeader>
+			</Card>
+		</div>
+	)
 }
 
 export default SignInPage
