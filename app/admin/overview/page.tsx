a5576fb35630c8c47234bbe629a182fcf722894c
Michal Gawik <gawlik.zator@o2.pl>
2025-04-09 13:45:37 +0200

    create admin dashboard

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,5 +1,40 @@
-const AdminOverviewPage = () => {
-	return <div>over</div>
+import { auth } from '@/auth'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getEventsSummary } from '@/lib/actions/event.action'
+import { Metadata } from 'next'
+import { GrWorkshop } from 'react-icons/gr'
+
+export const metadata: Metadata = {
+	title: 'Admin',
+}
+
+const AdminOverviewPage = async () => {
+	const session = await auth()
+
+	console.log(session)
+
+	if (session?.user?.role !== 'admin') {
+		throw new Error('User must be admin')
+	}
+
+	const summary = await getEventsSummary()
+
+	return (
+		<div className='space-y-2'>
+			<h1 className='text-xl font-bold'>Panel Administratora</h1>
+			<div className='w-1/2'>
+				<Card>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>Warsztaty</CardTitle>
+						<GrWorkshop />
+					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>{summary}</div>
+					</CardContent>
+				</Card>
+			</div>
+		</div>
+	)
 }
 
 export default AdminOverviewPage
--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -2,6 +2,7 @@
 
 import { PrismaClient } from '@prisma/client'
 import { convertToPlainObject } from '../utils'
+import { Prisma } from '@prisma/client'
 
 // Get latest events
 
@@ -11,4 +12,26 @@ export async function getLatestEvents() {
 	const data = await prisma.event.findMany()
 
 	return convertToPlainObject(data)
+}
+
+type SalesDataType = {
+	month: string
+	totalSales: number
+}[]
+
+export async function getEventsSummary() {
+	const prisma = new PrismaClient()
+
+	const eventCount = await prisma.event.count()
+
+	// const salesDataRaw = await prisma.$queryRaw<
+	// 	Array<{ month: string; totalEvents: Prisma.Decimal }>
+	// >`SELECT to_char("eventDate", 'MM/YY') as "month", sum("eventC") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`
+
+	// const salesData: SalesDataType = salesDataRaw.map(entry => ({
+	// 	month: entry.month,
+	// 	totalEvents: eventCount,
+	// }))
+
+	return eventCount
 }
