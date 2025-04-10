1006dc5b6a61a06c92325f8d378d9482b614a94b
Michal Gawik <gawlik.zator@o2.pl>
2025-04-10 12:10:31 +0200

    create admin anv and render

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 // import Menu from '@/components/shared/header/menu'
-// import MainNav from './main-nav'
+import MainNav from './main-nav'
 // import AdminSearch from '@/components/admin/admin-search'
 import smallLogo from '@/public/images/Miasto-Pracownia_Logo_small.png'
 import UserButton from '@/components/UserButton'
@@ -16,14 +16,13 @@ export default function AdminLayout({
 			<div className='flex flex-col'>
 				<div className='border-b container mx-auto'>
 					<div className='flex items-center justify-between h-16 px-4'>
-						<Link href='/' className='w-22'>
-							<Image src={smallLogo} height={150} width={150} alt={'logo'} />
-						</Link>
-						{/* <MainNav className='mx-6' />
-						<div className='ml-auto items-center flex space-x-4'>
-							<AdminSearch />
-							<Menu />
-						</div> */}
+						<div className='flex flex-row gap-x-12'>
+							<Link href='/' className='w-22'>
+								<Image src={smallLogo} height={150} width={150} alt={'logo'} />
+							</Link>
+							<MainNav className='mx-6' />
+						</div>
+
 						<UserButton />
 					</div>
 				</div>
