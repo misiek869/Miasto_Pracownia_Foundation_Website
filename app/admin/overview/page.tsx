65912519434ab2487edd3230e48456dce1838ac4
Michal Gawik <gawlik.zator@o2.pl>
2025-04-10 08:38:50 +0200

    create requireAdmin function

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/auth'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { getEventsSummary } from '@/lib/actions/event.action'
 import { Metadata } from 'next'
+import { redirect } from 'next/dist/server/api-utils'
 import { GrWorkshop } from 'react-icons/gr'
 
 export const metadata: Metadata = {
@@ -22,10 +23,28 @@ const AdminOverviewPage = async () => {
 	return (
 		<div className='space-y-2'>
 			<h1 className='text-xl font-bold'>Panel Administratora</h1>
-			<div className='w-1/2'>
+			<div className='grid grid-cols-3 gap-x-4'>
 				<Card>
 					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
 						<CardTitle className='text-sm font-medium'>Warsztaty</CardTitle>
+						<GrWorkshop />
+					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>{summary}</div>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>Zakończone</CardTitle>
+						<GrWorkshop />
+					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>{summary}</div>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>Przyszłe</CardTitle>
 						<GrWorkshop />
 					</CardHeader>
 					<CardContent>
