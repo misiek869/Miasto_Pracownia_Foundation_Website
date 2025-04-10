6b72246f84ff8b94755da4bb96922d637efceffe
Michal Gawik <gawlik.zator@o2.pl>
2025-04-10 09:27:53 +0200

    create details table

---
+++ app/admin/events/page.tsx
@@ -0,0 +1,81 @@
+import { requireAdmin } from '@/lib/auth-guard'
+import { auth } from '@/auth'
+import { getAllOrders } from '@/lib/actions/event.action'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table'
+
+export const metadata: Metadata = {
+	title: 'Admin Warsztaty',
+}
+
+const AdminEventsPage = async (props: {
+	searchParams: Promise<{ page: string }>
+}) => {
+	await requireAdmin()
+	const { page = '1' } = await props.searchParams
+	const session = await auth()
+
+	if (session?.user?.role !== 'admin') {
+		throw new Error('User must be admin')
+	}
+
+	const events = await getAllOrders({
+		page: Number(page),
+		limit: 2,
+	})
+
+	console.log(events)
+
+	return (
+		<div className='space-y-2'>
+			<h2 className='h2-bold'>Orders</h2>
+			<div className='overflow-x-auto'>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>ID</TableHead>
+							<TableHead>NAZWA</TableHead>
+							<TableHead>DATA</TableHead>
+							<TableHead>GODZINA</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{events.data.map(event => (
+							<TableRow key={event.id}>
+								{/* <TableCell>{formatId(order.id)}</TableCell> */}
+
+								{/* <TableCell>
+									{order.isPaid && order.paidAt
+										? formatDateTime(order.paidAt).dateTime
+										: 'Not Paid'}
+								</TableCell> */}
+
+								<TableCell>
+									<Link href={`/order/${event.id}`}>
+										<span className='px-2'>Details</span>
+									</Link>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				{/* {events.totalPages > 1 && (
+					<Pagination
+						page={Number(page) || 1}
+						totalPages={events?.totalPages}
+					/>
+				)} */}
+			</div>
+		</div>
+	)
+}
+
+export default AdminEventsPage
