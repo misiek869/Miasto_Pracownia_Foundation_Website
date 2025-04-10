3f6fef9d3316201dbf9e005ebd282dc7eb78a564
Michal Gawik <gawlik.zator@o2.pl>
2025-04-10 11:39:36 +0200

    c

---
+++ components/Pagination.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Button } from '../ui/button'
+import { formUrlQuery } from '@/lib/utils'
+
+type PaginationProps = {
+	page: number | string
+	totalPages: number
+	urlParamName?: string
+}
+
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+	const router = useRouter()
+	const searchParams = useSearchParams()
+
+	const handleClick = (btnType: string) => {
+		const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1
+		const newUrl = formUrlQuery({
+			params: searchParams.toString(),
+			key: urlParamName || 'page',
+			value: pageValue.toString(),
+		})
+
+		router.push(newUrl)
+	}
+
+	return <div></div>
+}
+
+export default Pagination
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import qs from 'query-string'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
@@ -11,4 +12,28 @@ export function convertToPlainObject<T>(value: T): T {
 
 export function formatId(id: string) {
 	return `..${id.substring(id.length - 6)}`
+}
+
+export function formUrlQuery({
+	params,
+	key,
+	value,
+}: {
+	params: string
+	key: string
+	value: string | null
+}) {
+	const query = qs.parse(params)
+
+	query[key] = value
+
+	return qs.stringifyUrl(
+		{
+			url: window.location.pathname,
+			query,
+		},
+		{
+			skipNull: true,
+		}
+	)
 }
