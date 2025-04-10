87498a043a18c1e3834f92874c389eca6b1aed0b
Michal Gawik <gawlik.zator@o2.pl>
2025-04-10 11:40:40 +0200

    create Pagination component

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -24,7 +24,26 @@ const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
 		router.push(newUrl)
 	}
 
-	return <div></div>
+	return (
+		<div className='flex gap-2'>
+			<Button
+				size='lg'
+				variant='outline'
+				className='w-28'
+				disabled={Number(page) <= 1}
+				onClick={() => handleClick('prev')}>
+				Previous
+			</Button>
+			<Button
+				size='lg'
+				variant='outline'
+				className='w-28'
+				disabled={Number(page) >= totalPages}
+				onClick={() => handleClick('next')}>
+				Next
+			</Button>
+		</div>
+	)
 }
 
 export default Pagination
