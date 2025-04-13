319836befce8c1dc09337f753c86bcf18fd77ede
Michal Gawik <gawlik.zator@o2.pl>
2025-04-13 06:38:47 +0200

    add 	id: z.string(), toinsertEventSchema

--- lib/validators.ts
+++ lib/validators.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 //schema for inserting events
 
 export const insertEventSchema = z.object({
+	id: z.string(),
 	name: z.string().min(1, { message: 'Nazwa jest wymagana' }),
 	color: z.string().min(1, { message: 'Kolor jest wymagany' }),
 	signUpUrl: z.string().min(1, { message: 'URL zapisów jest wymagany' }),
