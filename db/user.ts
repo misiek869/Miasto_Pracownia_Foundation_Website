450a67c174f8839e69c71e6ed092fdcb8835e12f
Michal Gawik <gawlik.zator@o2.pl>
2025-04-15 05:38:23 +0200

    getEnvVar create

--- db/user.ts
+++ db/user.ts
@@ -1,8 +1,14 @@
 import { hashSync } from 'bcrypt-ts-edge'
 
+function getEnvVar(name: string): string {
+	const value = process.env[name]
+	if (!value) throw new Error(`Missing environment variable: ${name}`)
+	return value
+}
+
 export const sampleUser = {
-	name: process.env.ADMIN_NAME,
-	email: process.env.ADMIN_EMAIL,
-	password: hashSync(process.env.ADMIN_INITIAL_PASSWORD, 10),
-	role: 'admin',
+	name: getEnvVar('ADMIN_NAME'),
+	email: getEnvVar('ADMIN_EMAIL'),
+	password: hashSync(getEnvVar('ADMIN_INITIAL_PASSWORD'), 10),
+	role: 'admin' as const,
 }
