8e2ee0bf52008f4bdc69c954cfd2dc4918b9be12
Michal Gawik <gawlik.zator@o2.pl>
2025-04-16 05:55:30 +0200

    try to use env t store sig in data

--- db/user.ts
+++ db/user.ts
@@ -1,14 +1,22 @@
 import { hashSync } from 'bcrypt-ts-edge'
+import 'dotenv/config'
 
-function getEnvVar(name: string): string {
-	const value = process.env[name]
-	if (!value) throw new Error(`Missing environment variable: ${name}`)
-	return value
-}
+// function getEnvVar(name: string): string {
+// 	const value = process.env[name]
+// 	if (!value) throw new Error(`Missing environment variable: ${name}`)
+// 	return value
+// }
 
 export const sampleUser = {
-	name: getEnvVar('ADMIN_NAME'),
-	email: getEnvVar('ADMIN_EMAIL'),
-	password: hashSync(getEnvVar('ADMIN_INITIAL_PASSWORD'), 10),
-	role: 'admin' as const,
+	name: process.env.ADMIN_NAME as string,
+	email: process.env.ADMIN_EMAIL as string,
+	password: process.env.ADMIN_INITIAL_PASSWORD,
+	role: 'admin' as string,
 }
+
+// export const sampleUser = {
+// 	name: process.env.ADMIN_NAME as string,
+// 	email: process.env.ADMIN_EMAIL as string,
+// 	password: hashSync(process.env.ADMIN_INITIAL_PASSWORD as string, 10),
+// 	role: 'admin' as string,
+// }
