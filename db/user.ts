3f9b99c55880ab641bd8a800ef0cca98ca73a9c0
Michal Gawik <gawlik.zator@o2.pl>
2025-04-17 20:53:16 +0200

    uncomment changes

--- db/user.ts
+++ db/user.ts
@@ -1,22 +1,15 @@
 import { hashSync } from 'bcrypt-ts-edge'
 import 'dotenv/config'
 
-// function getEnvVar(name: string): string {
-// 	const value = process.env[name]
-// 	if (!value) throw new Error(`Missing environment variable: ${name}`)
-// 	return value
-// }
-
-export const sampleUser = {
-	name: process.env.ADMIN_NAME as string,
-	email: process.env.ADMIN_EMAIL as string,
-	password: process.env.ADMIN_INITIAL_PASSWORD,
-	role: 'admin' as string,
+function getEnvVar(name: string): string {
+	const value = process.env[name]
+	if (!value) throw new Error(`Missing environment variable: ${name}`)
+	return value
 }
 
-// export const sampleUser = {
-// 	name: process.env.ADMIN_NAME as string,
-// 	email: process.env.ADMIN_EMAIL as string,
-// 	password: hashSync(process.env.ADMIN_INITIAL_PASSWORD as string, 10),
-// 	role: 'admin' as string,
-// }
+export const sampleUser = {
+	name: getEnvVar('ADMIN_NAME'),
+	email: getEnvVar('ADMIN_EMAIL'),
+	password: hashSync(getEnvVar('ADMIN_INITIAL_PASSWORD'), 10),
+	role: 'admin' as string,
+}
