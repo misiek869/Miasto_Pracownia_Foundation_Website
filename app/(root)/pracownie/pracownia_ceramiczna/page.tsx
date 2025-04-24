8862ae4eefa02917005629ea58d3e971347068a1
Michal Gawik <gawlik.zator@o2.pl>
2025-04-24 05:53:32 +0200

    add description

--- app/(root)/pracownie/pracownia_ceramiczna/page.tsx
+++ app/(root)/pracownie/pracownia_ceramiczna/page.tsx
@@ -9,6 +9,19 @@ const CeramicsWorkshopPage = () => {
 				<Navigation />
 			</div>
 			<PageTitle title='pracownia ceramiczna' />
+			<div className='mt-10 lg:mt-20 max-w-[80vw] md:max-w-[60vw] mx-auto mb-20'>
+				<h3 className='w-full text-justify font-montserrat tracking-wide leading-loose text-lg lg:text-xl mx-auto'>
+					Warsztaty ceramiczne, podczas których odkrywamy magię pracy z gliną –
+					od pierwszego dotyku, przez lepienie, aż po szkliwienie i wypalanie w
+					piecu. Pod opieką doświadczonej instruktorki uczymy się podstaw
+					ceramiki oraz poznajemy różne techniki formowania i zdobienia. Każdy
+					uczestnik – niezależnie od wieku czy umiejętności – może stworzyć coś
+					pięknego i unikalnego: miseczkę, figurkę, ozdobę, a nawet elementy
+					dekoracyjne do domu. To nie tylko kreatywne zajęcia, ale też chwila
+					wyciszenia, skupienia i satysfakcji z pracy własnych rąk. Ceramika
+					uczy cierpliwości i zachwytu nad procesem tworzenia.
+				</h3>
+			</div>
 			<Footer />
 		</>
 	)
--- app/(root)/pracownie/pracownia_kobiet/page.tsx
+++ app/(root)/pracownie/pracownia_kobiet/page.tsx
@@ -9,6 +9,20 @@ const WomanWorkshopPage = () => {
 				<Navigation />
 			</div>
 			<PageTitle title='pracownia kobiet' />
+			<div className='mt-10 lg:mt-20 max-w-[80vw] md:max-w-[60vw] mx-auto mb-20'>
+				<h3 className='w-full text-justify font-montserrat tracking-wide leading-loose text-lg lg:text-xl mx-auto'>
+					Przestrzeń stworzona z myślą o kobietach – pełna ciepła, zrozumienia i
+					wzajemnego wsparcia. Nasze warsztaty to coś więcej niż tylko zajęcia –
+					to czas dla Ciebie. Czas na rozwój, twórczość i autentyczne spotkanie
+					z innymi kobietami. W ramach Pracowni Kobiet zapraszamy na różnorodne
+					warsztaty: florystyczne, malarskie, rękodzielnicze, a także spotkania
+					oparte na psychologii i rozwoju osobistym. To przestrzeń, w której
+					możesz tworzyć, mówić, słuchać i być wysłuchaną – bez oceniania, w
+					zaufaniu i akceptacji. Nie musisz nic umieć ani niczego udowadniać –
+					wystarczy, że jesteś. My zadbamy o atmosferę, inspirację i czułą
+					obecność.
+				</h3>
+			</div>
 			<Footer />
 		</>
 	)
