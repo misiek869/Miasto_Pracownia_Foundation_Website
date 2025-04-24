481f714ccda0926f39a9046b1a8632068ecc339a
Michal Gawik <gawlik.zator@o2.pl>
2025-04-24 08:30:34 +0200

    add more description text

--- app/(root)/pracownie/pracownia_miasto/page.tsx
+++ app/(root)/pracownie/pracownia_miasto/page.tsx
@@ -20,6 +20,16 @@ const CityWorkshopPage = () => {
 					pytamy uczestników, jakie tematy, techniki czy pomysły chcieliby
 					wspólnie realizować – a potem działamy razem, by je urzeczywistnić.
 				</h3>
+
+				<h3 className='w-full text-justify font-montserrat tracking-wide leading-loose text-lg lg:text-xl mx-auto mt-4'>
+					W ramach projektu „Pracownia w mieście” działa otwarta inicjatywa,
+					podczas której można się z nami spotkać, porozmawiać przy kawie i
+					podzielić swoim pomysłem na twórcze działania w mieście. Jeśli masz
+					pasję, umiejętności i chcesz się nimi podzielić – jesteśmy tu, by
+					pomóc Ci rozwinąć skrzydła. Dzięki takim spotkaniom powstała u nas
+					Pracownia Krawiecka i Pracownia Ceramiki – kobiety, które zgłosiły się
+					z pomysłem, dziś prowadzą własne warsztaty dla innych.
+				</h3>
 			</div>
 			<Footer />
 		</>
