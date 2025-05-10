baa7d062c26982b7938acfda397b030b5569b84a
Michal Gawik <gawlik.zator@o2.pl>
2025-05-10 07:32:10 +0200

    add configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '2mb',
+			allowedOrigins: [],
+		},
+	},
+	output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
