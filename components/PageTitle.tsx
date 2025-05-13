dff262c708941fbd928a11dfea08d07670b6b5b8
Michal Gawik <gawlik.zator@o2.pl>
2025-05-13 11:10:02 +0200

    add bgColor prop

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const PageTitle = ({ title }: { title: string }) => {
+const PageTitle = ({
+	title,
+	bgColor = 'bg-gray-900',
+}: {
+	title: string
+	bgColor?: string
+}) => {
 	return (
 		<div className='flex'>
-			<h1 className='mx-auto text-center  font-calistoga tracking-widest leading-snug text-2xl sm:text-3xl lg:text-5xl text-slate-50 bg-gray-900 uppercase px-10 md:px-20 py-2 rounded-full shadow-xl'>
+			<h1
+				className={`mx-auto text-center  font-calistoga tracking-widest leading-snug text-2xl sm:text-3xl lg:text-5xl text-slate-50 ${bgColor} uppercase px-10 md:px-20 py-2 rounded-full shadow-xl`}>
 				{title}
 			</h1>
 		</div>
